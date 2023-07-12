904703586a62e862ab19a5b55ce997927dac312e
Ellie <61954992+Ellie72@users.noreply.github.com>
2023-07-12 01:26:48 -0300

    a

--- avancado/bancos.php
+++ avancado/bancos.php
@@ -1,34 +1,40 @@
-<?php 
-function sacar(array $conta, float $valorAsacar): array{
-    if($valorAsacar>$conta['saldo']){
-        exibeMensagem("Você não pode sacar");
-    } else{
-        $conta['saldo']-=$valorAsacar;
-    }
-    return $conta;
-}
-function debositar(array $conta, float $valorADepositar): array{
-    if($valorADepositar>0){
-        $conta['saldo'] += $valorADepositar;
-    } else{
-        exibeMensagem("Depositor precisam ser positivos");
-    return $conta;
-}
-function exibeMensagem(string $mensagem) {
-    echo $mensagem . PHP_EOL;
-}
+<?php
+
+require_once 'funcoes.php';
+
 $contasCorrentes = [
-    '1231231231231' => ['titular'=>'Vicinius', 'Saldo'=>1000],
-    '1231231232312' => ['titular'=>'Maria', 'Saldo'=>10000],
-    '3123122231123' => ['titular'=>'Roberto', 'Saldo'=>300],
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.456.689-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
 ];
-$contasCorrentes['1231231232312'] = sacar($contasCorrentes['1231231232312'], valorAsacar:500);
-$contasCorrentes['3123122231123'] = sacar($contasCorrentes['3123122231123'], valorAsacar:500);
-$contasCorrentes['1231231231231'] = debositar($contasCorrentes['1231231231231'], valorADepositar:500);
 
+$contasCorrentes['123.456.789-10'] = sacar(
+    $contasCorrentes['123.456.789-10'],
+    500
+);
 
-foreach ($contasCorrentes as $cpf => $conta){
+$contasCorrentes['123.456.689-11'] = sacar(
+    $contasCorrentes['123.456.689-11'],
+    200
+);
+
+$contasCorrentes['123.256.789-12'] = depositar(
+    $contasCorrentes['123.256.789-12'],
+    900
+);
+titularComLetrasMaiusculas($contasCorrentes['123.256.789-12']);
+
+foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem(
-        mensagem:"$cpf {$conta['titular']} {$conta['saldo']}"
+        "$cpf {$conta['titular']} {$conta['titular']}"
     );
 }
